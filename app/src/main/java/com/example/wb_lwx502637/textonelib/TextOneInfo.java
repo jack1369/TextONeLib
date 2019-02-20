daa21ec198146876cfd98498ddb9ab1037f241e8
lingfeng <wb-lwx502637@antfin.com>
2019-02-20 15:21:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wb_lwx502637/textonelib/TextOneInfo.java
@@ -0,0 +1,16 @@
+package com.example.wb_lwx502637.textonelib;
+
+import android.util.Log;
+
+/**
+ * Created by wb-lwx502637 on 2019/2/20.
+ */
+
+public class TextOneInfo {
+    public TextOneInfo() {
+
+    }
+    public void output() {
+        Log.e("TextOneInfo", "output:exeport " );
+    }
+}
