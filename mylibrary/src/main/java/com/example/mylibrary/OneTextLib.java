08f439798d6b2d831cfb0820d26d918235e345b3
lingfeng <wb-lwx502637@antfin.com>
2019-02-20 15:52:46 +0800

    测试

---
+++ mylibrary/src/main/java/com/example/mylibrary/OneTextLib.java
@@ -0,0 +1,16 @@
+package com.example.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by wb-lwx502637 on 2019/2/20.
+ */
+
+public class OneTextLib {
+    public OneTextLib() {
+
+    }
+    public void addLog() {
+        Log.e("OneTextLib", "addLog: OneTextLib" );
+    }
+}
